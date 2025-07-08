e10cf361dd0a288395de014cd112190670abb4d7
Renato Sortino <renato.sortino@diffuse.ly>
2025-07-08 12:10:01 +0200

    fix: change names in make_filelist.sh

--- make_filelist.sh
+++ make_filelist.sh
@@ -2,7 +2,7 @@ BASE_DIR="data/stepho_renderings/reflections_4"
 DATA_SPLITS_DIR="data_split/reflection"
 
 TRAIN_DIR="${BASE_DIR}/train"
-TRAIN_MASK_DIR="${BASE_DIR}/reflection_masks_otsu"
+TRAIN_MASK_DIR="${BASE_DIR}/train_masks"
 
 VAL_DIR="${BASE_DIR}/val"
 VAL_MASK_DIR="${BASE_DIR}/val_masks"
@@ -18,8 +18,8 @@ cat fnames.txt | while read fname;  do printf "${fname}.jpg\t${fname}_mask.jpg\n
 
 # Copy the images and masks to the merged directory
 echo "Copying images and masks to ${DST_TRAIN_DIR}"
-cat fnames.txt | while read fname;  do cp ${TRAIN_DIR}/${fname}.jpg ${DST_TRAIN_DIR}/${fname}.jpg ; done
-cat fnames.txt | while read fname;  do cp ${TRAIN_MASK_DIR}/${fname}.jpg ${DST_TRAIN_DIR}/${fname}_mask.jpg ; done
+cat fnames.txt | while read fname;  do cp ${TRAIN_DIR}/${fname}.jpg ${DST_TRAIN_DIR}/${fname}_orig.jpg ; cp ${TRAIN_DIR}/${fname}_cut.jpg ${DST_TRAIN_DIR}/${fname}_cut.jpg; done
+cat fnames.txt | while read fname;  do cp ${TRAIN_MASK_DIR}/${fname}_mask.jpg ${DST_TRAIN_DIR}/${fname}_mask.jpg ; cp ${TRAIN_MASK_DIR}/${fname}_reflection.jpg ${DST_TRAIN_DIR}/${fname}_reflection.jpg; done
 
 rm fnames.txt
 
@@ -28,7 +28,8 @@ cat fnames.txt | while read fname;  do printf "${fname}.jpg\t${fname}_mask.jpg\n
 
 # Copy the images and masks to the merged directory
 echo "Copying images and masks to ${DST_VAL_DIR}"
-cat fnames.txt | while read fname;  do cp ${VAL_DIR}/${fname}.jpg ${DST_VAL_DIR}/${fname}.jpg ; done
-cat fnames.txt | while read fname;  do cp ${VAL_MASK_DIR}/${fname}.jpg ${DST_VAL_DIR}/${fname}_mask.jpg ; done
+cat fnames.txt | while read fname;  do cp ${VAL_DIR}/${fname}.jpg ${DST_VAL_DIR}/${fname}_orig.jpg ; cp ${VAL_DIR}/${fname}_cut.jpg ${DST_VAL_DIR}/${fname}_cut.jpg; done
+cat fnames.txt | while read fname;  do cp ${VAL_MASK_DIR}/${fname}_mask.jpg ${DST_VAL_DIR}/${fname}_mask.jpg ; cp ${VAL_MASK_DIR}/${fname}_reflection.jpg ${DST_VAL_DIR}/${fname}_reflection.jpg; done
+
 
 rm fnames.txt
